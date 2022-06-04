9d0faf3d339ca7240be7d2d3bab3d3ccd2878c51
Baebang <wowjd2524@gmail.com>
2022-06-04 16:07:51 +0900

    Add: Create Mypage

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,30 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { firestore } from "./firebase";
+
+import { useRecoilValue } from "recoil";
+import { setUid } from "../recoil/loginCheck";
+import ListPage from "../components/ListPage";
 
 function Mypage() {
-  return <>sddfsdfsd</>;
+  const UidCheck = useRecoilValue(setUid);
+  console.log(UidCheck);
+
+  const CounterDB = firestore
+    .collection("Mypage")
+    .doc(UidCheck)
+    .collection("History");
+
+  console.log();
+
+  return (
+    <ListPage
+      variant="catalog"
+      title="면접 시뮬레이션 결과"
+      description="누적된 데이터를 기반으로 면접의 달인이 되어보세요!"
+    >
+      <br />
+    </ListPage>
+  );
 }
 
 export default Mypage;
-
-
